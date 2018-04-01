2fe1d6dd751371b277a574cb91f347cf377a1bca
Patrick Ziller <patrickandstuff@gmail.com>
2018-04-01 15:38:53 -0400

    add swiper and 3 screens with different checkpoints

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Swiper from "react-native-swiper";
 import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
 import BreathTimer from "./src/components/BreathTimer";
 import LinearGradient from "react-native-linear-gradient";
@@ -15,7 +16,48 @@ export default class App extends Component<Props> {
         colors={["#f2fcfe", "#1c92d2"]}
         style={[styles.container]}
       >
-        <BreathTimer
+        <Swiper style={styles.wrapper} showsButtons={false}>
+          <View style={styles.slide1}>
+            <BreathTimer
+              size={width}
+              outerRingRadius={width / 2 - 40}
+              width={5}
+              fontSize={20}
+              numberOfDots={2}
+              // timerRotation={0}
+              duration={DURATION}
+              initalScaleFactor={SCALE_FACTOR * 0.6}
+              endScaleFactor={SCALE_FACTOR * 1.13}
+            />
+          </View>
+          <View style={styles.slide2}>
+            <BreathTimer
+              size={width}
+              outerRingRadius={width / 2 - 40}
+              width={5}
+              fontSize={20}
+              numberOfDots={3}
+              // timerRotation={0}
+              duration={DURATION}
+              initalScaleFactor={SCALE_FACTOR * 0.6}
+              endScaleFactor={SCALE_FACTOR * 1.13}
+            />
+          </View>
+          <View style={styles.slide3}>
+            <BreathTimer
+              size={width}
+              outerRingRadius={width / 2 - 40}
+              width={5}
+              fontSize={20}
+              numberOfDots={4}
+              // timerRotation={0}
+              duration={DURATION}
+              initalScaleFactor={SCALE_FACTOR * 0.6}
+              endScaleFactor={SCALE_FACTOR * 1.13}
+            />
+          </View>
+        </Swiper>
+        {/* <BreathTimer
           size={width}
           outerRingRadius={width / 2 - 40}
           width={5}
@@ -25,13 +67,32 @@ export default class App extends Component<Props> {
           duration={DURATION}
           initalScaleFactor={SCALE_FACTOR * 0.6}
           endScaleFactor={SCALE_FACTOR * 1.13}
-        />
+        /> */}
       </LinearGradient>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#97CAE5",
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#92BBD9",
+  },
   spacer: {
     padding: 10,
   },
@@ -39,7 +100,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingLeft: 15,
-    paddingRight: 15,
+    // paddingLeft: 15,
+    // paddingRight: 15,
   },
 });
